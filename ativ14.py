a89f68563cedef107e110b3c20983cd6782373ad
Evellyn20242008 <Evellynmartins579@gmail.com>
2024-09-16 13:09:47 -0300

    atividade14

---
+++ ativ14.py
@@ -0,0 +1,23 @@
+# Crie um programa que receba três notas de um aluno e calcule a média. Informe se o aluno foi aprovado, reprovado ou se está de recuperação. Use as seguintes regras:
+# Média ≥ 7: Aprovado
+# 5 ≤ Média < 7: Recuperação
+# Média < 5: Reprovado
+n1 = float(input("digite a nota1"))
+n2 = float(input("digite a nota 2"))
+n3 = float(input("digite a nota 3"))
+ 
+ # calcule a media 
+ 
+ média =(n1 + n2 + n3) / 3
+print (f"a média é {média}")
+
+#aprovaçao 
+
+if média >=7:
+ print("aprovado")
+elif 5 <= média <7:
+ print("recuperaçao")
+
+elif média <5:
+ 
+ print("reprovado")
